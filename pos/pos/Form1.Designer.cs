f6b7701f53e152cc79c48ba4d5cd5acf7e5e8102
ryjolliffe <jolliffe.ryan@gmail.com>
2017-08-26 15:03:55 +1200

    Add files via upload

---
+++ pos/pos/Form1.Designer.cs
@@ -0,0 +1,339 @@
+﻿namespace pos
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.textBoxSubProperty2Price = new System.Windows.Forms.TextBox();
+            this.textBoxSubProperty3Price = new System.Windows.Forms.TextBox();
+            this.textBoxSubPropert1Price = new System.Windows.Forms.TextBox();
+            this.buttonAddStock = new System.Windows.Forms.Button();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.textBoxSubProperty3 = new System.Windows.Forms.TextBox();
+            this.textBoxSubProperty2 = new System.Windows.Forms.TextBox();
+            this.textBoxSubProperty1 = new System.Windows.Forms.TextBox();
+            this.textBoxNewStockItem = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.tabControl2 = new System.Windows.Forms.TabControl();
+            this.tabPage3 = new System.Windows.Forms.TabPage();
+            this.tabPage4 = new System.Windows.Forms.TabPage();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            this.tabControl2.SuspendLayout();
+            this.tabPage3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Location = new System.Drawing.Point(12, 12);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(1011, 669);
+            this.tabControl1.TabIndex = 0;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.textBoxSubProperty2Price);
+            this.tabPage1.Controls.Add(this.textBoxSubProperty3Price);
+            this.tabPage1.Controls.Add(this.textBoxSubPropert1Price);
+            this.tabPage1.Controls.Add(this.buttonAddStock);
+            this.tabPage1.Controls.Add(this.radioButton2);
+            this.tabPage1.Controls.Add(this.radioButton1);
+            this.tabPage1.Controls.Add(this.textBoxSubProperty3);
+            this.tabPage1.Controls.Add(this.textBoxSubProperty2);
+            this.tabPage1.Controls.Add(this.textBoxSubProperty1);
+            this.tabPage1.Controls.Add(this.textBoxNewStockItem);
+            this.tabPage1.Controls.Add(this.label4);
+            this.tabPage1.Controls.Add(this.label3);
+            this.tabPage1.Controls.Add(this.label2);
+            this.tabPage1.Controls.Add(this.label1);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(1003, 643);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Control Panel";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // textBoxSubProperty2Price
+            // 
+            this.textBoxSubProperty2Price.Location = new System.Drawing.Point(245, 109);
+            this.textBoxSubProperty2Price.Name = "textBoxSubProperty2Price";
+            this.textBoxSubProperty2Price.Size = new System.Drawing.Size(100, 20);
+            this.textBoxSubProperty2Price.TabIndex = 14;
+            // 
+            // textBoxSubProperty3Price
+            // 
+            this.textBoxSubProperty3Price.Location = new System.Drawing.Point(245, 147);
+            this.textBoxSubProperty3Price.Name = "textBoxSubProperty3Price";
+            this.textBoxSubProperty3Price.Size = new System.Drawing.Size(100, 20);
+            this.textBoxSubProperty3Price.TabIndex = 13;
+            // 
+            // textBoxSubPropert1Price
+            // 
+            this.textBoxSubPropert1Price.Location = new System.Drawing.Point(245, 70);
+            this.textBoxSubPropert1Price.Name = "textBoxSubPropert1Price";
+            this.textBoxSubPropert1Price.Size = new System.Drawing.Size(100, 20);
+            this.textBoxSubPropert1Price.TabIndex = 12;
+            // 
+            // buttonAddStock
+            // 
+            this.buttonAddStock.Location = new System.Drawing.Point(9, 236);
+            this.buttonAddStock.Name = "buttonAddStock";
+            this.buttonAddStock.Size = new System.Drawing.Size(217, 23);
+            this.buttonAddStock.TabIndex = 11;
+            this.buttonAddStock.Text = "Add Stock Item";
+            this.buttonAddStock.UseVisualStyleBackColor = true;
+            this.buttonAddStock.Click += new System.EventHandler(this.buttonAddStock_Click);
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(141, 200);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(50, 17);
+            this.radioButton2.TabIndex = 10;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "Drink";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(10, 200);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(49, 17);
+            this.radioButton1.TabIndex = 9;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Food";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // textBoxSubProperty3
+            // 
+            this.textBoxSubProperty3.Location = new System.Drawing.Point(126, 147);
+            this.textBoxSubProperty3.Name = "textBoxSubProperty3";
+            this.textBoxSubProperty3.Size = new System.Drawing.Size(100, 20);
+            this.textBoxSubProperty3.TabIndex = 8;
+            // 
+            // textBoxSubProperty2
+            // 
+            this.textBoxSubProperty2.Location = new System.Drawing.Point(126, 109);
+            this.textBoxSubProperty2.Name = "textBoxSubProperty2";
+            this.textBoxSubProperty2.Size = new System.Drawing.Size(100, 20);
+            this.textBoxSubProperty2.TabIndex = 7;
+            // 
+            // textBoxSubProperty1
+            // 
+            this.textBoxSubProperty1.Location = new System.Drawing.Point(126, 70);
+            this.textBoxSubProperty1.Name = "textBoxSubProperty1";
+            this.textBoxSubProperty1.Size = new System.Drawing.Size(100, 20);
+            this.textBoxSubProperty1.TabIndex = 6;
+            // 
+            // textBoxNewStockItem
+            // 
+            this.textBoxNewStockItem.Location = new System.Drawing.Point(126, 35);
+            this.textBoxNewStockItem.Name = "textBoxNewStockItem";
+            this.textBoxNewStockItem.Size = new System.Drawing.Size(100, 20);
+            this.textBoxNewStockItem.TabIndex = 5;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(7, 150);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(70, 13);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Subproperty3";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(6, 109);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(71, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "SubProperty2";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 70);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(71, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "SubProperty1";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 35);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(114, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "New Stock Item Name";
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.tabControl2);
+            this.tabPage2.Controls.Add(this.listBox1);
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(1003, 643);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Food & Drink";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // tabControl2
+            // 
+            this.tabControl2.Controls.Add(this.tabPage3);
+            this.tabControl2.Controls.Add(this.tabPage4);
+            this.tabControl2.Location = new System.Drawing.Point(487, 20);
+            this.tabControl2.Name = "tabControl2";
+            this.tabControl2.SelectedIndex = 0;
+            this.tabControl2.Size = new System.Drawing.Size(510, 617);
+            this.tabControl2.TabIndex = 1;
+            // 
+            // tabPage3
+            // 
+            this.tabPage3.Controls.Add(this.button3);
+            this.tabPage3.Controls.Add(this.button2);
+            this.tabPage3.Controls.Add(this.button1);
+            this.tabPage3.Location = new System.Drawing.Point(4, 22);
+            this.tabPage3.Name = "tabPage3";
+            this.tabPage3.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage3.Size = new System.Drawing.Size(502, 591);
+            this.tabPage3.TabIndex = 0;
+            this.tabPage3.Text = "Food";
+            this.tabPage3.UseVisualStyleBackColor = true;
+            // 
+            // tabPage4
+            // 
+            this.tabPage4.Location = new System.Drawing.Point(4, 22);
+            this.tabPage4.Name = "tabPage4";
+            this.tabPage4.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage4.Size = new System.Drawing.Size(502, 591);
+            this.tabPage4.TabIndex = 1;
+            this.tabPage4.Text = "Drink";
+            this.tabPage4.UseVisualStyleBackColor = true;
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(6, 18);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(461, 615);
+            this.listBox1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(6, 15);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(107, 53);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(119, 15);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(107, 53);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(241, 15);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(107, 53);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1035, 693);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            this.tabPage2.ResumeLayout(false);
+            this.tabControl2.ResumeLayout(false);
+            this.tabPage3.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.Button buttonAddStock;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.TextBox textBoxSubProperty3;
+        private System.Windows.Forms.TextBox textBoxSubProperty2;
+        private System.Windows.Forms.TextBox textBoxSubProperty1;
+        private System.Windows.Forms.TextBox textBoxNewStockItem;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.TabControl tabControl2;
+        private System.Windows.Forms.TabPage tabPage3;
+        private System.Windows.Forms.TabPage tabPage4;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.TextBox textBoxSubProperty2Price;
+        private System.Windows.Forms.TextBox textBoxSubProperty3Price;
+        private System.Windows.Forms.TextBox textBoxSubPropert1Price;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ pos/pos/NewStockItem.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pos
+{
+    class NewStockItem
+    {
+        string _itemName;
+        string _subProperty1;
+        string _subProperty2;
+        string _subproperty3;
+        int _subProperty1Price;
+        int _subProperty2Price;
+        int _subProperty3Price;
+
+        public string ItemName
+        {
+            get {return _itemName; }
+            set {_itemName = value; }
+        }
+        public string SubProp1
+        {
+            get { return _subProperty1; }
+            set { _subProperty1 = value; }
+        }
+        public string SubProp2
+        
+        {
+            get { return _subProperty2; }
+            set { _subProperty2 = value; }
+        }
+        public string SubProp3
+        {
+            get { return _subproperty3; }
+            set {_subproperty3 = value; }
+        }
+        public int SubProp1Price1
+        {
+            get {return _subProperty1Price; }
+            set {_subProperty1Price = value; }
+        }
+        public int SubProp1Price2
+        {
+            get { return _subProperty2Price; }
+            set { _subProperty2Price = value; }
+        }
+        public int SubProp1Price3
+        {
+            get { return _subProperty3Price; }
+            set { _subProperty3Price = value; }
+        }
+
+        public NewStockItem(string _itemname, string _subPropert1, int _subProprty1Price, string _subPropert2, int _subPropert2Price, string _subProperty3, int _subProprty3Price)
+
+    }
+}
